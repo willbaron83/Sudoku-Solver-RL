fd8667b5417d67ab40d6e812782a86781c52bef5
Emma Peatfield <emma.peatfield@sjsu.edu>
2020-04-25 16:24:19 -0700

    Can check if grid is solved

--- sudoku_solve.py
+++ sudoku_solve.py
@@ -11,13 +11,14 @@ from gym import spaces
 import random, copy
 from pprint import pprint as pp
 import numpy as np
+import math
 import sys
 
 resolved = 0
 unfinished = 1
 error = 2
 
-solved_grid = [[3,4,1,2,9,7,6,8,5],
+solved_grid = np.array( [[3,4,1,2,9,7,6,8,5],
               [2,5,6,8,3,4,9,7,1],
               [9,8,7,1,5,6,3,2,4],
               [1,9,2,6,7,5,8,4,3],
@@ -25,9 +26,9 @@ solved_grid = [[3,4,1,2,9,7,6,8,5],
               [6,3,4,9,1,8,2,5,7],
               [5,6,3,7,8,9,4,1,2],
               [4,1,9,5,6,2,7,3,8],
-              [7,2,8,3,4,1,5,6,9]]
+              [7,2,8,3,4,1,5,6,9]])
 
-unsolved_grid = [[9,4,1,2,9,7,6,8,5],
+unsolved_grid = np.array([[9,4,1,2,9,7,6,8,5],
                   [2,5,6,8,3,4,9,7,1],
                   [9,8,7,1,5,6,3,2,4],
                   [1,9,2,6,6,5,8,4,3],
@@ -35,29 +36,55 @@ unsolved_grid = [[9,4,1,2,9,7,6,8,5],
                   [6,3,4,9,1,8,2,5,7],
                   [5,6,3,7,8,9,4,1,2],
                   [4,1,9,5,6,2,2,3,8],
-                  [7,2,8,3,4,1,5,6,9]]
+                  [7,2,8,3,4,1,5,6,9]])
 
 
 def checkIfSolved(grid):
+    N = len(grid)
     #check rows
-    for i in range(9):
-        row = grid[i]
-        for i in range(9):
-            count = row.count(i+1)
-            if not count == 1:
-                return False
-    #check columns
-    for column in range(9):
-        col = []
-        for row in range(9):
-            col.append(grid[row][column])
-        for i in range(9):
-            count = col.count(i+1)
-            if not count == 1: 
-                return False
-    #check boxes        
-    print("SOLVED")
+    for i in range(1, N + 1):
+        count = np.count_nonzero(grid == i, axis=1)
+        valid = checkOnesInside(count)
+        if not valid:
+            return False
+        countcols = np.count_nonzero(grid == i, axis = 0)
+        validcols = checkOnesInside(countcols)
+        if not validcols:
+            return False
+
+    #check boxes
+    N = len(grid)
+    j = int(math.sqrt(N))
+    k = N//j
+    m = 0
+    rowm = m
+    rowk = k
+    colm = m
+    colk = k
+    while colk < N+1:
+        #for i range(j):
+        rowk = k
+        rowm = m
+        while rowk < N+1:
+            box = []
+            for col in range(colm, colk):
+                for row in range(rowm, rowk):
+                    value = grid[row][col]
+                    box.append(value)
+            for i in range(9):
+                count = box.count(i+1)
+                if not count == 1:
+                    return False
+            rowm += j
+            rowk += j
+        colk += j
+        colm += j
+        
     return True
+
+def checkOnesInside(array):
+    return all(x == array[0] for x in array)
+
 
 # def getSolutions(grid):
 #     isSolved = checkIfSolved(grid)
