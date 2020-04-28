db9205f0223c0c77d5cf558bb5d555939ac87297
Emma Peatfield <emma.peatfield@sjsu.edu>
2020-04-27 22:42:57 -0700

    Step & Reset, now need to connect test.py & sudoku_solve

--- sudoku_solve.py
+++ sudoku_solve.py
@@ -35,86 +35,119 @@ unsolved_grid = np.array([[9,4,1,2,9,7,6,8,5],
                   [4,1,9,5,6,2,2,3,8],
                   [7,2,8,3,4,1,5,6,9]])
 
-
-def checkIfSolved(grid):
-    N = len(grid)
-    #check rows
-    for i in range(1, N + 1):
-        count = np.count_nonzero(grid == i, axis=1)
-        valid = checkOnesInside(count)
-        if not valid:
-            return False
-        countcols = np.count_nonzero(grid == i, axis = 0)
-        validcols = checkOnesInside(countcols)
-        if not validcols:
-            return False
-
-    #check boxes
-    N = len(grid)
-    j = int(math.sqrt(N))
-    k = N//j
-    m = 0
-    rowm = m
-    rowk = k
-    colm = m
-    colk = k
-    while colk < N+1:
-        #for i range(j):
-        rowk = k
-        rowm = m
-        while rowk < N+1:
-            box = []
-            for col in range(colm, colk):
-                for row in range(rowm, rowk):
-                    value = grid[row][col]
-                    box.append(value)
-            for i in range(9):
-                count = box.count(i+1)
-                if not count == 1:
-                    return False
-            rowm += j
-            rowk += j
-        colk += j
-        colm += j
-        
-    return True
-
-def checkOnesInside(array):
-    return all(x == array[0] for x in array)
-
-# @author: https://github.com/artonge/gym-sudoku
-def getSolutions(grid, stop=1, col=-1, row=-1, omit=-1):
-    isSolved = checkIfSolved(grid)
-    if isSolved:
-        return grid
+class Sudoku_Solve():
+    last_action = None
     
-    #check for empty spaces if no i given in 
-    if col == -1:
-        for col in range(N):
-            for row in range(N):
-                if grid[col][row] == 0:
-                    break
-            if grid[col][row] == 0: 
-                break
+    def __init__(self):
+        self.last_action = None
+        self.grid = []
+        self.base = getSolutions(np.zeros(shape=(9,9)))[0]
+    
+    def checkIfSolved(grid):
+        N = len(grid)
+        zeros = np.count_nonzero(grid == 0)
+        if not zeros == 0:
+            return -1
+        #check rows
+        for i in range(1, N + 1):
+            count = np.count_nonzero(grid == i, axis=1)
+            valid = checkOnesInside(count)
+            if not valid:
+                return 0
+            countcols = np.count_nonzero(grid == i, axis = 0)
+            validcols = checkOnesInside(countcols)
+            if not validcols:
+                return 0
+    
+        #check boxes
+        N = len(grid)
+        j = int(math.sqrt(N))
+        k = N//j
+        m = 0
+        rowm = m
+        rowk = k
+        colm = m
+        colk = k
+        while colk < N+1:
+            #for i range(j):
+            rowk = k
+            rowm = m
+            while rowk < N+1:
+                box = []
+                for col in range(colm, colk):
+                    for row in range(rowm, rowk):
+                        value = grid[row][col]
+                        box.append(value)
+                for i in range(9):
+                    count = box.count(i+1)
+                    if not count == 1:
+                        return 0
+                rowm += j
+                rowk += j
+            colk += j
+            colm += j
             
-    values = np.arange(1, N+1)
-    np.random.shuffle(values)
-    solutions = np.empty(shape=(0,N,N))
-    for value in values:
-        if omit == value:
-            continue
-        copy = np.copy(grid)
-        copy[col][row] = value
-        subSol = getSolutions(copy, stop=stop-len(solutions))
-        solutions = np.concatenate((solutions, subSol))
-        if len(solutions) >= stop:
-            return solutions
-    return solutions
+        return 1
+    
+    def checkOnesInside(array):
+        return all(x == array[0] for x in array)
+    
+    # @author: https://github.com/artonge/gym-sudoku
+    def getSolutions(grid, stop=1, col=-1, row=-1, omit=-1):
+        isSolved = checkIfSolved(grid)
+        if isSolved == 1:
+            return grid
         
-#def step()
-#def reset()
+        #check for empty spaces if no i given in 
+        if col == -1:
+            for col in range(N):
+                for row in range(N):
+                    if grid[col][row] == 0:
+                        break
+                if grid[col][row] == 0: 
+                    break
+                
+        values = np.arange(1, N+1)
+        np.random.shuffle(values)
+        solutions = np.empty(shape=(0,N,N))
+        for value in values:
+            if omit == value:
+                continue
+            copy = np.copy(grid)
+            copy[col][row] = value
+            subSol = getSolutions(copy, stop=stop-len(solutions))
+            solutions = np.concatenate((solutions, subSol))
+            if len(solutions) >= stop:
+                return solutions
+        return solutions
+            
+    def step(self, action):
+        self.last_action = action
+        last_grid = np.copy(self.grid)
         
-print(checkIfSolved(solved_grid))
-print(checkIfSolved(unsolved_grid))
-#def checkInVector(self, grid):
+        if self.grid[action[0]][action[1]] != 0:
+            return np.copy(self.grid), -1, False, None
+        
+        self.grid[action[0]][action[1]] = action[2]+1
+        
+        sol = checkIfSolved(self.grid)
+        
+        if sol == -1:
+            return np.copy(self.grid), 1, False, None
+        if sol == 1:
+            return np.copy(self.grid), 1, True, None
+        if sol == 0: 
+            return np.copy(self.grid), -1, False, None
+        
+        
+        
+    def reset(self):
+        print("______________Hello")
+        self.last_action = None
+        self.grid = np.copy(self.base)
+        return np.copy(self.grid)
+            
+  # print(checkIfSolved(solved_grid))
+   #print(checkIfSolved(unsolved_grid))
+    #def checkInVector(self, grid):
     
---
+++ test.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 22:12:34 2020
+
+@author: epeatfield
+"""
+
+from gym import spaces
+from sudoku_solve import Sudoku_Solve
+import numpy as np
+
+max_episodes = 5
+steps_per_episode = 20
+epsilon_min = 0.005
+max_num_steps = max_episodes * steps_per_episode
+epsilon_decay = 500 * epsilon_min /max_num_steps
+alpha = 0.05
+gamma = 0.98
+num_discrete_bins = 30
+
+class Q_Learner_Sudoku():
+    def __init__(self):
+        self.obs_shape = spaces.Box(low=1, high=9, shape=(9, 9))
+        self.obs_bins = num_discrete_bins
+        self.action_shape =  spaces.Tuple((spaces.Discrete(9), spaces.Discrete(9), spaces.Discrete(9)))
+        
+        self.Q = np.zeros((self.obs_bins+1, self.obs_bins+1, self.action_shape))
+        
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = 1.0
+        
+    def get_action(self, obs):
+        if self.epsilon > epsilon_min: 
+            self.epsilon -= epsilon_decay
+        if np.random.random() > self.epsilon:
+            return np.argmax(selfQ[obs])
+        else:
+            return np.random.choice([a for a in range(self.action_shape)])
+        
+    def learn(self, obs, action, reward, next_obs):
+        td_target = reward + self.gamma * np.max(self.Q[next_obs])
+        td_error = td_target - self.Q[obs][action]
+        self.Q[obs][action] += self.alpha * td_error        
+        
+def train(agent):
+    best_reward = -float('inf')
+    for episode in range(max_episodes):
+        done = False
+        obs = Sudoku_Solve.reset()
+        total = 0.0
+        while not done:
+            action = agent.get_action(obs)
+            next_obs, reward,done, info = Sudoku_Solve.step(action)
+            agent.learn(obs, action, reward, next_obs)
+            obs = next_obs
+            total += reward
+        if total > best_reward:
+            best_reward = total
+        print("Episode#:{} reward:{} best_reward:{} eps:{}".format(episode,
+                                     total_reward, best_reward, agent.epsilon))
+        
+def test(agent, policy):
+    done = False
+    obs = Sudoku_Solve.reset()
+    total_reward = 0.0
+    while not done:
+        action = policy[obs]
+        next_obs, reward, done, info = Sudoku_Solve.step(action)
+        obs = next_obs
+        total_reward += reward
+    return total_reward
+
+if __name__ == "main":
+    agent = Q_Learner_Sudoku()
+    learned_policy = train(agent)
+    print(learned_policy)
+    
+    
+        
