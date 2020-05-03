5fc90d017d68ec4f442499e26758108ad5ce3bcd
willbaron83 <william.baron@sjsu.edu>
2020-05-02 18:01:05 -0700

    newest changes made. This needs a library found here, there are changes to the files that need to be done, basically just the // instead of the / and the range instead of xrange. there are not many changes in the file. This still has some issues, we can talk about them when we meet

--- sudoku_solve.py
+++ sudoku_solve.py
@@ -3,149 +3,99 @@
 """
 Created on Sat Apr 25 13:34:52 2020
 
-@author: epeatfield
+@author: wbaron
 """
 
 import gym
-from gym import spaces
-import random, copy
-from pprint import pprint as pp
+import gym_sudoku
 import numpy as np
-import math
-import sys
 
-solved_grid = np.array([[3, 4, 1, 2, 9, 7, 6, 8, 5],
-                        [2, 5, 6, 8, 3, 4, 9, 7, 1],
-                        [9, 8, 7, 1, 5, 6, 3, 2, 4],
-                        [1, 9, 2, 6, 7, 5, 8, 4, 3],
-                        [8, 7, 5, 4, 2, 3, 1, 9, 6],
-                        [6, 3, 4, 9, 1, 8, 2, 5, 7],
-                        [5, 6, 3, 7, 8, 9, 4, 1, 2],
-                        [4, 1, 9, 5, 6, 2, 7, 3, 8],
-                        [7, 2, 8, 3, 4, 1, 5, 6, 9]])
-
-unsolved_grid = np.array([[9, 4, 1, 2, 9, 7, 6, 8, 5],
-                          [2, 5, 6, 8, 3, 4, 9, 7, 1],
-                          [9, 8, 7, 1, 5, 6, 3, 2, 4],
-                          [1, 9, 2, 6, 6, 5, 8, 4, 3],
-                          [8, 7, 5, 4, 2, 3, 1, 9, 6],
-                          [6, 3, 4, 9, 1, 8, 2, 5, 7],
-                          [5, 6, 3, 7, 8, 9, 4, 1, 2],
-                          [4, 1, 9, 5, 6, 2, 2, 3, 8],
-                          [7, 2, 8, 3, 4, 1, 5, 6, 9]])
+#MAX_NUM_EPISODES = 500
+MAX_NUM_EPISODES = 500
+STEPS_PER_EPISODE = 200 #  This is specific to MountainCar. May change with env
+EPSILON_MIN = 0.005
+max_num_steps = MAX_NUM_EPISODES * STEPS_PER_EPISODE
+EPSILON_DECAY = 500 * EPSILON_MIN / max_num_steps
+ALPHA = 0.05  # Learning rate
+GAMMA = 0.98  # Discount factor
+NUM_DISCRETE_BINS = 30  # Number of bins to Discretize each observation dim
 
 
-class Sudoku_Solve():
-    last_action = None
+class Q_Learner(object):
+    def __init__(self, env):
+        self.obs_shape = env.observation_space.shape
+        self.obs_high = env.observation_space.high
+        self.obs_low = env.observation_space.low
+        self.obs_bins = NUM_DISCRETE_BINS  # Number of bins to Discretize each observation dim
+        self.bin_width = (self.obs_high - self.obs_low) / self.obs_bins
+        self.action_shape = env.action_space.spaces
+        # Create a multi-dimensional array (aka. Table) to represent the
+        # Q-values
+        self.Q = np.zeros((self.obs_bins + 1, self.obs_bins + 1,
+                           self.action_shape))  # (51 x 51 x 3)
+        self.alpha = ALPHA  # Learning rate
+        self.gamma = GAMMA  # Discount factor
+        self.epsilon = 1.0
 
-    def __init__(self):
-        self.last_action = None
-        self.grid = []
-        self.base = getSolutions(np.zeros(shape=(9, 9)))[0]
+    def discretize(self, obs):
+        return tuple(((obs - self.obs_low) / self.bin_width).astype(int))
 
-    def checkIfSolved(grid):
-        N = len(grid)
-        zeros = np.count_nonzero(grid == 0)
-        if not zeros == 0:
-            return -1
-        # check rows
-        for i in range(1, N + 1):
-            count = np.count_nonzero(grid == i, axis=1)
-            valid = checkOnesInside(count)
-            if not valid:
-                return 0
-            countcols = np.count_nonzero(grid == i, axis=0)
-            validcols = checkOnesInside(countcols)
-            if not validcols:
-                return 0
+    def get_action(self, obs):
+        discretized_obs = self.discretize(obs)
+        # Epsilon-Greedy action selection
+        if self.epsilon > EPSILON_MIN:
+            self.epsilon -= EPSILON_DECAY
+        if np.random.random() > self.epsilon:
+            return np.argmax(self.Q[discretized_obs])
+        else:  # Choose a random action
+            return np.random.choice([a for a in range(self.action_shape)])
 
-        # check boxes
-        N = len(grid)
-        j = int(math.sqrt(N))
-        k = N // j
-        m = 0
-        rowm = m
-        rowk = k
-        colm = m
-        colk = k
-        while colk < N + 1:
-            # for i range(j):
-            rowk = k
-            rowm = m
-            while rowk < N + 1:
-                box = []
-                for col in range(colm, colk):
-                    for row in range(rowm, rowk):
-                        value = grid[row][col]
-                        box.append(value)
-                for i in range(9):
-                    count = box.count(i + 1)
-                    if not count == 1:
-                        return 0
-                rowm += j
-                rowk += j
-            colk += j
-            colm += j
+    def learn(self, obs, action, reward, next_obs):
+        discretized_obs = self.discretize(obs)
+        discretized_next_obs = self.discretize(next_obs)
+        td_target = reward + self.gamma * np.max(self.Q[discretized_next_obs])
+        td_error = td_target - self.Q[discretized_obs][action]
+        self.Q[discretized_obs][action] += self.alpha * td_error
 
-        return 1
+def train(agent, env):
+    best_reward = -float('inf')
+    for episode in range(MAX_NUM_EPISODES):
+        done = False
+        obs = env.reset()
+        total_reward = 0.0
+        while not done:
+            action = agent.get_action(obs)
+            next_obs, reward, done, info = env.step(action)
+            agent.learn(obs, action, reward, next_obs)
+            obs = next_obs
+            total_reward += reward
+        if total_reward > best_reward:
+            best_reward = total_reward
+        print("Episode#:{} reward:{} best_reward:{} eps:{}".format(episode,
+                                     total_reward, best_reward, agent.epsilon))
+    # Return the trained policy
+    return np.argmax(agent.Q, axis=2)
 
-    def checkOnesInside(array):
-        return all(x == array[0] for x in array)
 
-    # @author: https://github.com/artonge/gym-sudoku
-    def getSolutions(grid, stop=1, col=-1, row=-1, omit=-1):
-        isSolved = checkIfSolved(grid)
-        if isSolved == 1:
-            return grid
+def test(agent, env, policy):
+    done = False
+    obs = env.reset()
+    total_reward = 0.0
+    while not done:
+        action = policy[agent.discretize(obs)]
+        next_obs, reward, done, info = env.step(action)
+        obs = next_obs
+        total_reward += reward
+    return total_reward
 
-        # check for empty spaces if no i given in
-        if col == -1:
-            for col in range(N):
-                for row in range(N):
-                    if grid[col][row] == 0:
-                        break
-                if grid[col][row] == 0:
-                    break
 
-        values = np.arange(1, N + 1)
-        np.random.shuffle(values)
-        solutions = np.empty(shape=(0, N, N))
-        for value in values:
-            if omit == value:
-                continue
-            copy = np.copy(grid)
-            copy[col][row] = value
-            subSol = getSolutions(copy, stop=stop - len(solutions))
-            solutions = np.concatenate((solutions, subSol))
-            if len(solutions) >= stop:
-                return solutions
-        return solutions
-
-    def step(self, action):
-        self.last_action = action
-        last_grid = np.copy(self.grid)
-
-        if self.grid[action[0]][action[1]] != 0:
-            return np.copy(self.grid), -1, False, None
-
-        self.grid[action[0]][action[1]] = action[2] + 1
-
-        sol = checkIfSolved(self.grid)
-
-        if sol == -1:
-            return np.copy(self.grid), 1, False, None
-        if sol == 1:
-            return np.copy(self.grid), 1, True, None
-        if sol == 0:
-            return np.copy(self.grid), -1, False, None
-
-    def reset(self):
-        print("______________Hello")
-        self.last_action = None
-        self.grid = np.copy(self.base)
-        return np.copy(self.grid)
-
-# print(checkIfSolved(solved_grid))
-# print(checkIfSolved(unsolved_grid))
-# def checkInVector(self, grid):
-
+if __name__ == "__main__":
+    env = gym.make('Sudoku-v0')
+    agent = Q_Learner(env)
+    learned_policy = train(agent, env)
+    # Use the Gym Monitor wrapper to evalaute the agent and record video
+    gym_monitor_path = "./gym_monitor_output"
+    env = gym.wrappers.Monitor(env, gym_monitor_path, force=True)
+    for _ in range(1000):
+        test(agent, env, learned_policy)
+    env.close()
