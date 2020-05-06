2ba26d0fe1684507bcd5d7a92ced0a9aaddcc014
willbaron83 <william.baron@sjsu.edu>
2020-05-06 16:42:04 -0700

    forgot to add the changes on this file

--- sudoku_solve.py
+++ sudoku_solve.py
@@ -10,6 +10,35 @@ from q_learner import train
 from q_learner import test
 from dqn_solver import DQNSolver
 
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+
+
+def sudoku_keras_rl():
+    env = SudokuEnv(9)
+    # np.random.seed(123)
+    # env.seed(123)
+    nb_actions = env.action_space.n+1
+    model = Sequential()
+    model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_actions))
+    model.add(Activation('linear'))
+    print(model.summary())
+    policy = EpsGreedyQPolicy()
+    memory = SequentialMemory(limit=50000, window_length=1)
+    dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10, target_model_update=1e-2, policy=policy)
+    dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+    dqn.fit(env, nb_steps=5000, visualize=True, verbose=2)
+    dqn.test(env, nb_episodes=5, visualize=True)
+
 # from scores.score_logger import ScoreLogger
 
 def dqn_sudoku():
@@ -57,3 +86,4 @@ def q_learning_sudoku():
 if __name__ == "__main__":
     # dqn_sudoku()
     q_learning_sudoku()
+    sudoku_keras_rl()
