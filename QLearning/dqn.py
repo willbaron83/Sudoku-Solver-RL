13e8741766976c80bb406d3e0625e1d850380afb
Emma Peatfield <emma.peatfield@sjsu.edu>
2020-05-16 10:41:04 -0700

    Remove comments

--- QLearning/dqn.py
+++ QLearning/dqn.py
@@ -68,10 +68,8 @@ class DQNSolver:
 
 
 def sudoku(n):
-    # env = gym.make(ENV_NAME)
     env = SudokuEnv(n)
     details = {}
-    # score_logger = ScoreLogger(ENV_NAME)
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
     dqn_solver = DQNSolver(observation_space, action_space)
@@ -80,14 +78,11 @@ def sudoku(n):
     while run < 100:
         run += 1
         state = env.reset()
-        # print(state)
-        # print(observation_space)
         state = np.reshape(state, [observation_space, observation_space])
         step = 0
         total_reward = 0.0
         while True:
             step += 1
-            #env.render()
             action = dqn_solver.act(state)
             state_next, reward, terminal, info = env.step(action)
             reward = reward if not terminal else -reward
@@ -95,20 +90,16 @@ def sudoku(n):
             dqn_solver.remember(state, action, reward, state_next, terminal)
             state = state_next
             total_reward += reward
-            # print(state)
             if terminal:
                 if total_reward > best_reward:
                     best_reward = total_reward
                 details[f"trial{run}"] = {"run": run, "steps": step, "totalreward": reward, "best_reward": best_reward, "exploration": dqn_solver.exploration_rate}  
                 print("Run: " + str(run) + ", exploration: " + str(dqn_solver.exploration_rate) + ", score: " + str(step))
-                # score_logger.add_score(step, run)
                 break
-            # print(details)
             dqn_solver.experience_replay()
     #save deatils tp a file
     with open(f"DataValue{n}.json", "w") as f:
         json.dump(details,f)
 
 if __name__ == "__main__":
-    # for n in range(3):
     sudoku(3)
